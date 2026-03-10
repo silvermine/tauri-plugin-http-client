358a3729135cfb8ad59573aa05c8455b31142eb1
Jordan Hafer <jordan.hafer+github@gmail.com>
2026-03-09 21:10:17 -0400

    sub(feat): add Tauri app as example

---
+++ examples/tauri-app/src-tauri/src/lib.rs
@@ -0,0 +1,18 @@
+use std::time::Duration;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+   tauri::Builder::default()
+      .plugin(
+         tauri_plugin_http_client::Builder::new()
+            .allowed_domains(vec![
+               "httpbin.org".into(),
+               "*.httpbin.org".into(),
+            ])
+            .default_timeout(Duration::from_secs(30))
+            .max_response_body_size(5 * 1024 * 1024)
+            .build(),
+      )
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
+}
