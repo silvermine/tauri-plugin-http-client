a27eccf7e45ed94395c6aa5724722e64efdcf5ba
Hafer, Jordan <jhafer@bethel.jw.org>
2026-03-12 14:55:13 -0400

    feat: add http request support
    Includes the following features:
    
    * A dynamic 'allowlist' which can be configured at
    initialization and during an app session.
    
    * Retry support.
    
    See README for more information.

---
+++ guest-js/index.ts
@@ -0,0 +1,9 @@
+export { request } from './http-client';
+export { HttpHeaders } from './headers';
+export { HttpClientError, HttpErrorCode } from './errors';
+export type {
+   RequestOptions,
+   HttpResponse,
+   HttpMethod,
+   BodyInit,
+} from './types';
